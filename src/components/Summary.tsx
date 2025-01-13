614908e805574f33b20a2e8ee9245b5cfbcafedb
Sophia See <sophiasee11@gmail.com>
2025-01-14 00:29:37 +0800

    updated fonts

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -4,7 +4,7 @@ export default function Summary () {
     return (
         <div className="flex flex-col gap-6 px-[32px] py-[24px] flex-1 md:gap-7 md:py-[38px]">
             {/* summary title */}
-            <div className="font-bold text-[18px] text-navyblue">Summary</div>
+            <div className="font-bold text-[18px] text-navyblue md:text-[24px]">Summary</div>
             {/* summary cards */}
             <div className="flex flex-col gap-4">
                 {data.map((category) => {
@@ -15,11 +15,11 @@ export default function Summary () {
                         <div className={`flex justify-between rounded-[12px] ${bgColor} py-[17px] px-[16px]`}>
                             <div className="flex gap-3">
                                 <img src={`${category.icon}`} alt={`icon for ${category.category}`} />
-                                <div className={`${textColor}`}>{category.category}</div>
+                                <div className={`${textColor} md:text-[18px] font-medium`}>{category.category}</div>
                             </div>
                             <div className="flex gap-2">
-                                <div className="font-bold text-[16px] text-navyblue">{category.score}</div>
-                                <div className="font-bold text-[16px] text-navyblue opacity-50">/ 100</div>
+                                <div className="font-bold text-[16px] text-navyblue md:text-[18px]">{category.score}</div>
+                                <div className="font-bold text-[16px] text-navyblue opacity-50 md:text-[18px]">/ 100</div>
                             </div>
                         </div>
                     )
